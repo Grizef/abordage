9a4454144d68e0b782469257c6896018c2c0f3ab
Grizef <arkan.catalon@laposte.net>
2025-05-13 13:22:51 +0200

    init
    Site pour l'association abord'age

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+    content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,md}'],
+    theme: {
+      extend: {
+        colors: {
+          primary: '#4C6FFF',
+          secondary: '#F9F9F9',
+          accent: '#FFD85C',
+        },
+      },
+    },
+    plugins: [],
+  };
+  
